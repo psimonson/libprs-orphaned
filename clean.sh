456a66ad7a76cd9ec6ae6c33bbdf73b7ba4b0ea2
Philip R. Simonson <psimonson1988@gmail.com>
2019-05-17 19:30:08 -0400

    Finally builds with automake

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm -rf Makefile Makefile.in aclocal.m4 autom4te.cache/ \
 compile config.log config.status config.h.in configure \
-depcomp install-sh missing ar-lib src/Makefile.in \
-test/Makefile.in test-driver
+depcomp install-sh missing src/Makefile.in m4/ \
+test/Makefile.in test-driver ltmain.sh config.sub config.guess \
+ar-lib config.h libtool stamp-h1
