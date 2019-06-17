826c6e6128bd4b1253b423eab184cae298c0edd5
Philip R. Simonson <psimonson1988@gmail.com>
2019-06-17 00:31:33 -0400

    Finished converting to CMake instead of using autotools

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ if [ "$?" = "0" ]; then
 	rm -rf build
 fi
 
-#cmake -H. -Bbuild -DBUILD_TESTS=ON && cmake --build build --target all \
-#&& cmake --build build --target test || die Error!
-
-cmake -H. -Bbuild && cmake --build build --target all
+if [ "$1" = "test" ]; then
+	cmake -H. -Bbuild -DBUILD_TESTS=ON && \
+	cmake --build build --target all \
+	&& cmake --build build --target test || die Error!
+else
+	cmake -H. -Bbuild && cmake --build build --target all
+fi
