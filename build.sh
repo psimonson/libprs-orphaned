c183c9eb55a093e095bef9b1aaacd43e82164e2b
Philip Simonson <psimonson1988@gmail.com>
2019-06-19 16:18:30 -0400

    Added install directory to build.sh

--- build.sh
+++ build.sh
@@ -3,21 +3,24 @@ test -d build
 if [ "$?" = "0" ]; then
 	rm -rf build
 fi
-
-if [ "$1" = "test" ]; then
-	cmake -H. -Bbuild -DBUILD_TESTS=ON && \
+install_dir=-DCMAKE_INSTALL_PREFIX=testing
+if [ ! "$1" = "" ]; then
+	install_dir=-DCMAKE_INSTALL_PREFIX=$1
+fi
+if [ "$2" = "test" ]; then
+	cmake -H. -Bbuild -DBUILD_TESTS=ON $install_dir && \
 	cmake --build build --target all \
 	&& cmake --build build --target test || die Error!
-elif [ "$1" = "win32-test" ]; then
+elif [ "$2" = "win32-test" ]; then
 	cmake -H. -Bbuild -DBUILD_TESTS=ON \
 	-DCMAKE_TOOLCHAIN_FILE=Ubuntu-mingw_w64-toolchain.cmake && \
 	cmake --build build --target all && \
 	cmake --build build --target test || exit
-elif [ "$1" = "win32" ]; then
+elif [ "$2" = "win32" ]; then
 	cmake -H. -Bbuild \
 	-DCMAKE_TOOLCHAIN_FILE=Ubuntu-mingw_w64-toolchain.cmake && \
 	cmake --build build --target all || exit
 else
-	cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=testing && \
+	cmake -H. -Bbuild $install_dir && \
 	cmake --build build --target all
 fi
