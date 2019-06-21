168f9199a0293722c0da6d118055ef38b79681dc
Philip Simonson <psimonson1988@gmail.com>
2019-06-21 08:53:32 -0400

    Fixed vector.h

--- include/vector.h
+++ include/vector.h
@@ -1,50 +1,140 @@
-/**
- * @file vector.h
- * @author Philip R. Simonson
- * @date 20 June 2019
- * @brief Header file for a very simple vector implementation.
- * @details
- *
- * This is a vector implementation like that of the C++ vector
- * implementation.
- */
-
 #ifndef PRS_VECTOR_H
 #define PRS_VECTOR_H
 
-#include "export.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <assert.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/**
+ * @brief For internal use only.
+ */
+#define _vector_set_capacity(vec, size) \
+do { \
+	if(vec) { \
+		((size_t*)(vec))[-1] = (size); \
+	} \
+} while(0)
 
-#define VECTOR_INIT_CAPACITY 4
+/**
+ * @brief For internal use only.
+ */
+#define _vector_set_size(vec, size) \
+do { \
+	if(vec) { \
+		((size_t*)(vec))[-2] = (size); \
+	} \
+} while(0)
 
-#define VECTOR_INIT(vec) vector_init(&vec)
-#define VECTOR_MAKE(type, name) name = malloc(sizeof(type)); if(!name) NULL
-#define VECTOR_ADD(vec, item) vector_add(&vec, (void*)item)
-#define VECTOR_SET(vec, id, item) vector_set(&vec, id, (void*)item)
-#define VECTOR_GET(vec, type, id) (type)vector_get(&vec, id)
-#define VECTOR_DELETE(vec, id) vector_delete(&vec, id)
-#define VECTOR_TOTAL(vec) vector_total(&vec)
-#define VECTOR_FREE(vec) vector_free(&vec)
+/**
+ * @brief Gets the current capacity of the vector.
+ */
+#define vector_capacity(vec) \
+	((vec) ? ((size_t*)(vec))[-1] : (size_t)0)
 
-typedef struct vector {
-	void **items;
-	int capacity;
-	int total;
-} vector_t;
+/**
+ * @brief Gets the current size of the vector.
+ */
+#define vector_size(vec) \
+	((vec) ? ((size_t*)(vec))[-2] : (size_t)0)
 
-PRS_EXPORT void vector_init(vector_t*);
-PRS_EXPORT int vector_total(vector_t*);
-PRS_EXPORT void vector_add(vector_t*, void*);
-PRS_EXPORT void vector_set(vector_t*, int, void*);
-PRS_EXPORT void *vector_get(vector_t*, int);
-PRS_EXPORT void vector_delete(vector_t*, int);
-PRS_EXPORT void vector_free(vector_t*);
+/**
+ * @brief Returns non-zero if the vector is empty.
+ */
+#define vector_empty(vec) \
+	(vector_size(vec) == 0)
 
-#ifdef __cplusplus
-}
+/**
+ * @brief For internal use only, grows vector.
+ */
+#define _vector_grow(vec, count) \
+do { \
+	if(!(vec)) { \
+		size_t *__p; \
+		__p = malloc((count)*sizeof(*(vec))+(sizeof(size_t)*2)); \
+		assert(__p); \
+		(vec) = (void*)(&__p[2]); \
+		_vector_set_capacity((vec), (count)); \
+		_vector_set_size((vec), 0); \
+	} else { \
+		size_t *__p1,*__p2; \
+		__p1 = &((size_t*)(vec))[-2]; \
+		__p2 = \
+		realloc(__p1, ((count)*sizeof(*(vec))+(sizeof(size_t)*2))); \
+		assert(__p2); \
+		(vec) = (void*)(&__p2[2]); \
+		_vector_set_capacity((vec), (count)); \
+	} \
+} while(0)
+
+/**
+ * @brief Removes the last element from the vector.
+ */
+#define vector_pop_back(vec) \
+do { \
+	_vector_set_size((vec), vector_size(vec)-1); \
+} while(0)
+
+/**
+ * @brief Removes the element at i index from the vector.
+ */
+#define vector_erase(vec, i) \
+do { \
+	if(vec) { \
+		const size_t __sz = vector_size(vec); \
+		if((i) < __sz) { \
+			size_t __x;
+			_vector_set_size((vec), __sz-1); \
+			for(__x=(i); __x < (__sz-1); ++__x) { \
+				(vec)[__x] = (vec)[__x+1]; \
+			} \
+		} \
+	} \
+} while(0)
+
+/**
+ * @brief Frees all memory associated with the vector.
+ */
+#define vector_free(vec) \
+do { \
+	if(vec) { \
+		size_t *p1 = &((size_t*)(vec))[-2]; \
+		free(p1); \
+	} \
+} while(0)
+
+/**
+ * @brief Returns an iterator to first element of the vector.
+ */
+#define vector_begin(vec) (vec)
+
+/**
+ * @brief Returns an iterator one past the last element of the vector.
+ */
+#define vector_end(vec) ((vec) ? &((vec)[vector_size(vec)]) : NULL)
+
+/**
+ * @brief Adds an element to the end of the vector.
+ */
+#ifdef LOGARITHMIC_GROWTH
+#define vector_push_back(vec, value) \
+do { \
+	size_t __cap = vector_capacity(vec); \
+	if(__cap <= vector_size(vec)) { \
+		_vector_grow((vec), !__cap ? __cap+1 : __cap*2); \
+	} \
+	vec[vector_size(vec)] = (value); \
+	_vector_set_size((vec), vector_size(vec)+1); \
+} while(0)
+#else
+#define vector_push_back(vec, value) \
+do { \
+	size_t __cap = vector_capacity(vec); \
+	if(__cap <= vector_size(vec)) { \
+		_vector_grow((vec), __cap+1); \
+	} \
+	vec[vector_size(vec)] = (value);
+	_vector_set_size((vec), vector_size(vec)+1); \
+} while(0)
 #endif
 
 #endif
