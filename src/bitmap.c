763d9b24041729a3380b94401767b7d272aa5187
Philip Simonson <psimonson1988@gmail.com>
2020-04-23 20:50:53 -0400

    Fixed my get/set pixel functions, now it doesn't segfault.

--- src/bitmap.c
+++ src/bitmap.c
@@ -162,14 +162,14 @@ PRS_EXPORT void get_pixel_bitmap(Bitmap *bmp, int y, int x, Color *pixel)
 	}
 	switch(check_endian()) {
 	case BIG_ENDIAN:
-		pixel->r = bmp->data[(bmp->info.width*y+x)*3].r;
-		pixel->g = bmp->data[(bmp->info.width*y+x)*3].g;
-		pixel->b = bmp->data[(bmp->info.width*y+x)*3].b;
+		pixel->r = bmp->data[bmp->info.width*y+x].r;
+		pixel->g = bmp->data[bmp->info.width*y+x].g;
+		pixel->b = bmp->data[bmp->info.width*y+x].b;
 	break;
 	case LITTLE_ENDIAN:
-		pixel->r = bmp->data[(bmp->info.width*y+x)*3].b;
-		pixel->g = bmp->data[(bmp->info.width*y+x)*3].g;
-		pixel->b = bmp->data[(bmp->info.width*y+x)*3].r;
+		pixel->r = bmp->data[bmp->info.width*y+x].b;
+		pixel->g = bmp->data[bmp->info.width*y+x].g;
+		pixel->b = bmp->data[bmp->info.width*y+x].r;
 	break;
 	default:
 		printf("Endian not supported.\n");
@@ -186,14 +186,14 @@ PRS_EXPORT void set_pixel_bitmap(Bitmap *bmp, int y, int x, Color pixel)
 	}
 	switch(check_endian()) {
 	case BIG_ENDIAN:
-		bmp->data[(bmp->info.width*y+x)*3].r = pixel.r;
-		bmp->data[(bmp->info.width*y+x)*3].g = pixel.g;
-		bmp->data[(bmp->info.width*y+x)*3].b = pixel.b;
+		bmp->data[bmp->info.width*y+x].r = pixel.r;
+		bmp->data[bmp->info.width*y+x].g = pixel.g;
+		bmp->data[bmp->info.width*y+x].b = pixel.b;
 	break;
 	case LITTLE_ENDIAN:
-		bmp->data[(bmp->info.width*y+x)*3].b = pixel.r;
-		bmp->data[(bmp->info.width*y+x)*3].g = pixel.g;
-		bmp->data[(bmp->info.width*y+x)*3].r = pixel.b;
+		bmp->data[bmp->info.width*y+x].b = pixel.r;
+		bmp->data[bmp->info.width*y+x].g = pixel.g;
+		bmp->data[bmp->info.width*y+x].r = pixel.b;
 	break;
 	default:
 		printf("Endian not supported.\n");
